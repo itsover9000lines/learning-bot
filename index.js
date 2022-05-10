46fffe726ac91158a049e3c046742a71e915ae32
itsover9000lines <oneillhb12@protonmail.com>
2022-05-09 20:36:29 -0400

    uncommented lines

--- index.js
+++ index.js
@@ -19,24 +19,24 @@ let bot = {
 client.commands = new Discord.Collection()
 client.events = new Discord.Collection()
 
-//
-// client.on("ready", () => {
-//     console.log(`Logged in as ${client.user.tag}`)
-// })
 
-// client.on("messageCreate", (message) => {
-//     if(message.content == "hi"){
-//         message.reply("Hello!")
-//     }
-// })
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
 
-// const welcomeChannelID = "531599666643468289"
-// client.on("guildMemberAdd", async (member) => {
-//     const img = await generateImage(member)
-//     member.guild.channels.cache.get(welcomeChannelID).send({
-//         content: `<@${member.id}> Welcome!`,
-//         files: [img]
-//     })
-// })
+client.on("messageCreate", (message) => {
+    if(message.content == "hi"){
+        message.reply("Hello!")
+    }
+})
+
+const welcomeChannelID = "531599666643468289"
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelID).send({
+        content: `<@${member.id}> Welcome!`,
+        files: [img]
+    })
+})
 
 client.login(process.env.TOKEN)
